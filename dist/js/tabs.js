6693267182674711705b99eb50c49b815dfdf0a7
Nic Oliver <nic@themezilla.com>
2015-06-19 12:00:12 -0700

    added callbacks

--- dist/js/tabs.js
+++ dist/js/tabs.js
@@ -5,7 +5,9 @@ export default class Tabs {
     this.$el = $('[data-tabs]');
 
     this.options = $.extend({
-      activeClass: 'active'
+      activeClass: 'active',
+      afterSetup: () => {},
+      afterChange: () => {}
     }, options);
 
     this._init();
@@ -19,8 +21,8 @@ export default class Tabs {
     this.$el.each((idx, el) => {
       const $tabUI = $(el);
       let $activeTab = $tabUI.find('a').eq(0);
-      let activeTabPanel = $activeTab.attr('href');
-      const $inactiveTabPanels = $(activeTabPanel).siblings();
+      let $activeTabPanel = $($activeTab.attr('href'));
+      const $inactiveTabPanels = $activeTabPanel.siblings();
 
       if (hash) {
         // Check if hash is a match to one of the tab panel IDs
@@ -30,14 +32,17 @@ export default class Tabs {
 
           if (tabId == hash) {
             $activeTab = this.$el.find(`a[href="${tabId}"]`);
-            activeTabPanel = tabId;
+            $activeTabPanel = $(tabId);
           }
         });
       }
 
       // Add active class to tab and panel
       $activeTab.parent().addClass(this.options.activeClass);
-      $(activeTabPanel).addClass(this.options.activeClass);
+      $activeTabPanel.addClass(this.options.activeClass);
+
+      // afterSetup callback
+      this.options.afterSetup.call();
     });
   }
 
@@ -54,6 +59,9 @@ export default class Tabs {
     window.location.hash = $target.attr('href');
     this._toggleClasses($target.parent());
     this._toggleClasses($($target.attr('href')));
+
+    // afterChange callback
+    this.options.afterChange.call();
   }
 
   _toggleClasses($el) {
